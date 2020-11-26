4208efae78c4efbe67f8be7999c6f7e644ece429
Tom Sommer <tomsommer@users.noreply.github.com>
2020-11-26 12:00:07 +0100

    Syntax

--- treesize.php
+++ treesize.php
@@ -14,9 +14,9 @@
     }
 
     $objects = new RecursiveIteratorIterator(
-            new RecursiveDirectoryIterator($path, RecursiveDirectoryIterator::SKIP_DOTS),
-            RecursiveIteratorIterator::SELF_FIRST,
-            RecursiveIteratorIterator::CATCH_GET_CHILD
+        new RecursiveDirectoryIterator($path, RecursiveDirectoryIterator::SKIP_DOTS),
+        RecursiveIteratorIterator::SELF_FIRST,
+        RecursiveIteratorIterator::CATCH_GET_CHILD
     );
 
     $dirlist = [];
@@ -32,29 +32,29 @@
 <!doctype html>
 <html>
 <head>
-    <meta charset="utf-8">
-	<meta name="robots" content="noindex">
-    <title>Treesize: <?php echo htmlentities($path) ?></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <meta charset="utf-8">
+  <meta name="robots" content="noindex">
+  <title>Treesize: <?php echo htmlentities($path) ?></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
 <div class="container-fluid">
-	<h1 class="display-4"><?php echo htmlentities($path) ?></h1>
-    <table class="table table-condensed table-hover table-striped">
-        <thead>
+  <h1 class="display-4"><?php echo htmlentities($path) ?></h1>
+  <table class="table table-condensed table-hover table-striped">
+    <thead>
+    <tr>
+      <th>Path</th>
+      <th class="text-right">Size</th>
+    </tr>
+    </thead>
+      <?php foreach ( $dirlist as $dir => $size ) { ?>
         <tr>
-            <th>Path</th>
-            <th class="text-right">Size</th>
+          <th class="text-monospace"><?php echo htmlentities($dir) ?></th>
+          <td class="text-right small text-nowrap"><?php echo number_format($size / 1024, 0, ',', '.') ?> KB</td>
         </tr>
-        </thead>
-        <?php foreach ( $dirlist as $dir => $size ) { ?>
-          <tr>
-            <th class="text-monospace"><?php echo htmlentities($dir) ?></th>
-            <td class="text-right small text-nowrap"><?php echo number_format($size / 1024, 0, ',', '.') ?> KB</td>
-          </tr>
-        <?php } ?>
-    </table>
+      <?php } ?>
+  </table>
 </div>
 </body>
 </html>
